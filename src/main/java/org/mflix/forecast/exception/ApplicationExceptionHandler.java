5543941f9daef3c531108084eb48713b7e1d52b6
Cciradih <mountain@cciradih.top>
2020-02-24 12:54:35 +0800

    参数验证及异常覆盖。

--- src/main/java/org/mflix/forecast/exception/ApplicationExceptionHandler.java
+++ src/main/java/org/mflix/forecast/exception/ApplicationExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 import org.mflix.forecast.component.ResponseComponent;
 import org.mflix.forecast.view.ResponseView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -26,5 +27,11 @@ public class ApplicationExceptionHandler {
     public ResponseEntity<ResponseView> handlerMethodArgumentNotValid(MethodArgumentNotValidException e) {
         String message = e.getBindingResult().getFieldError().getDefaultMessage().toUpperCase();
         return responseComponent.generate(2, message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ResponseView> hadlerEmptyResultDataAccessException(EmptyResultDataAccessException e) {
+        String message = e.getMessage().toUpperCase();
+        return responseComponent.generate(1, message, HttpStatus.NOT_FOUND);
     }
 }
